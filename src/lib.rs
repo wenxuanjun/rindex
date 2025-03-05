ce27327fb67759720440edfd430c826dac5e33ab
wenxuanjun <wenxuanjun@yahoo.com>
2025-03-05 12:23:13 +0800

    Use snmalloc for faster allocation

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,6 @@ mod service;
 pub use explorer::ExplorerEntry;
 pub use log::Log;
 pub use service::{QueryResult, Service};
+
+#[global_allocator]
+static GLOBAL: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
